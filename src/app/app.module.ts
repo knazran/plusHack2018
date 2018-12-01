c1fd01cf034502cc482bbf6b50aee4ce0b4965b5
Knazran <khairulnazran94@gmail.com>
2018-12-01 11:32:45 +0800

    Plussie talks now

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-import { NativeAudio } from '@ionic-native/native-audio/ngx'
+import { NativeAudio } from '@ionic-native/native-audio/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
--- src/app/start-trip/start-trip.page.ts
+++ src/app/start-trip/start-trip.page.ts
@@ -9,27 +9,110 @@ import { NativeAudio } from '@ionic-native/native-audio/ngx';
 })
 export class StartTripPage implements OnInit {
   take_break_audio: any;
+  rnr_audio: any;
+  congestion_audio: any;
+  redirect_audio: any;
+  thanks_audio: any;
+
+  plussie_count = 0;
+
+  take_a_break_audio: any;
   constructor(private toastCtrl: ToastController, private nativeAudio: NativeAudio,
               private platform: Platform) {
-    platform.ready().then(() => {
-      this.nativeAudio.preloadComplex('take_break_audio', '../../assets/take_a_break.mp3', 1, 1, 0);
-    });
   }
 
   ngOnInit() {
+    this.take_a_break_audio = new Audio();
+    this.take_a_break_audio.src = '../assets/take_a_break.mp3';
+    this.take_a_break_audio.load();
+
+    this.rnr_audio = new Audio();
+    this.rnr_audio.src = '../assets/lets_go_rnr.mp3';
+    this.rnr_audio.load();
+
+    this.congestion_audio = new Audio();
+    this.congestion_audio.src = '../assets/congestion_alert.mp3';
+    this.congestion_audio.load();
+
+    this.redirect_audio = new Audio();
+    this.redirect_audio.src = '../assets/redirect.mp3';
+    this.redirect_audio.load();
+
+    this.thanks_audio = new Audio();
+    this.thanks_audio.src = '../assets/thankyou.mp3';
+    this.thanks_audio.load();
   }
 
+
   async toastTakeABreak() {
-    this.nativeAudio.play('take_break_audio', () => {console.log('played'); toast.dismiss(); } );
-    const toast = await this.toastCtrl.create({
-      // tslint:disable-next-line:quotemark
-      message: "We have been driving for 2 hours. Let's take a break?",
-      position: 'bottom',
-      duration: 5000,
-      color: 'warning',
-      cssClass: 'plussie-talks'
-    });
-    toast.present();
+
+    // Hacky ASS shit FUCKING WHY
+    if (this.plussie_count === 0) {
+      this.take_a_break_audio.play();
+      const toast = await this.toastCtrl.create({
+        // tslint:disable-next-line:quotemark
+        message: "We have been driving for 2 hours. Let's take a break?",
+        position: 'bottom',
+        duration: 5000,
+        color: 'warning',
+        cssClass: 'plussie-talks'
+      });
+      toast.present();
+    }
+
+    if (this.plussie_count === 1) {
+      this.rnr_audio.play();
+      const toast = await this.toastCtrl.create({
+        // tslint:disable-next-line:quotemark
+        message: "Nearest RnR is Ayer Keroh. Let's go?",
+        position: 'bottom',
+        duration: 8000,
+        color: 'primary',
+        cssClass: 'plussie-talks'
+      });
+      toast.present();
+    }
+
+    if (this.plussie_count === 2) {
+      this.congestion_audio.play();
+      const toast = await this.toastCtrl.create({
+        // tslint:disable-next-line:quotemark
+        message: "Congestion head due to heavy rain.",
+        position: 'bottom',
+        duration: 10000,
+        color: 'danger',
+        cssClass: 'plussie-talks'
+      });
+      toast.present();
+    }
+
+    if (this.plussie_count === 3) {
+      this.redirect_audio.play();
+      const toast = await this.toastCtrl.create({
+        // tslint:disable-next-line:quotemark
+        message: "Redirecting to Exit 213. Will be arriving on time.",
+        position: 'bottom',
+        duration: 7000,
+        color: 'success',
+        cssClass: 'plussie-talks'
+      });
+      toast.present();
+    }
+
+    if (this.plussie_count === 4) {
+      this.thanks_audio.play();
+      const toast = await this.toastCtrl.create({
+        // tslint:disable-next-line:quotemark
+        message: "No problem",
+        position: 'bottom',
+        duration: 3000,
+        color: 'primary',
+        cssClass: 'plussie-talks'
+      });
+      toast.present();
+      this.plussie_count = 0
+    }
+    this.plussie_count = this.plussie_count + 1;
   }
 
 }
